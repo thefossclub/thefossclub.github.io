caefef5f5a31fc22583b25130e62bac7a9a56799
The FOSS Club <151220186+thefossclub@users.noreply.github.com>
2024-03-07 18:52:50 +0530

    added base files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
